95a34bdd83e2343deb405af5e23154964fbe6737
unknown <wahibrehman01@gmail.com>
2025-08-05 14:22:12 +0530

    first version of our hotels

---
+++ routes/personRoutes.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const router = express.Router();
+
+const Person = require('./../models/Person');
+
+router.post('/', async (req, res) => {
+  try{
+    const data = req.body
+
+    const newPerson = new Person(data);
+  
+    const response = await newPerson.save();
+    console.log('data saved');
+    res.status(200).json(response) 
+  }
+  catch(err){
+    console.log(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
+router.get('/', async (req, res) => {
+  try {
+    const data = await Person.find();
+    console.log('data fetched');
+    res.status(200).json(data);
+  }catch(err) {
+    console.log(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
+router.get('/:workType', async (req, res) => {
+  try {
+    const workType = req.params.workType;
+    if(workType == 'Chef' || workType == 'Waiter' || workType == 'Manager') {
+    const response = await Person.find({work: workType});
+    console.log('response fetched');
+    res.status(200).json(response);
+  }else{
+    res.status(404).json({ error: 'Invalid work type'});
+  }
+  }catch(err) {
+    console.log(err);
+    res.status(500).json({ error: 'Internal Server Error'});
+  }
+})
+
+router.put('/:d', async (req, res) =>{
+  try{
+    const personId = req.params.d;
+    const updatedPersonData = req.body;
+
+    const response = await Person.findByIdAndUpdate(personId, updatedPersonData, {
+      new: true,
+      runValidators: true,
+    })
+
+    if (!response) {
+      return res.status(404).json({ error: 'Person not found' });
+    }
+
+    console.log('data updated');
+    res.status(200).json(response);
+  }catch(err) {
+    console.log(err);
+    res.status(500).json({ error: 'Internal Server Error'})
+  }
+})
+
+router.delete('/:Id', async (req, res) =>{
+  try{
+    const personId = req.params.Id;
+    const response = await Person.findByIdAndDelete(personId);
+
+    if (!response) {
+      return res.status(404).json({ error: 'Person not found' });
+    }
+    console.log('data updated');
+    res.status(200).json({message: 'person Deleted Successfully'});
+  }catch(err){
+    console.log(err);
+    res.status(500).json({ error: 'Internam Server Error'});
+  }
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express();
+
+const db = require('./db');
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to our Hotel')
+})
+
+const personRoutes = require('./routes/personRoutes');
+const menuItemRoutes = require('./routes/MenuItemRoutes');
+
+app.use('/menu', menuItemRoutes);
+app.use('/person', personRoutes);
+
+app.listen(3000, () => {
+  console.log('listening on port 3000')
+})
